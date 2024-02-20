29a4a33ff9003cdaf299c977e110c5202cf39dac
Paul <01fpv@mail.ru>
2024-02-20 22:18:27 +0300

    create models

--- fastapi-solution/src/models/film.py
+++ fastapi-solution/src/models/film.py
@@ -1,20 +1,53 @@
+from datetime import date
+from typing import List, Optional
+from uuid import UUID
+
 import orjson
 
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
-from pydantic import BaseModel
+# Используем pydantic для упрощения работы
+# при перегонке данных из json в объекты
+from pydantic import AwareDatetime, BaseModel
 
 
 def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
+    # orjson.dumps возвращает bytes,
+    # а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
 
-class Film(BaseModel):
-    id: str
-    title: str
-    description: str
+class UuidMixin(BaseModel):
+    id: UUID
 
+
+class DateMixin(BaseModel):
+    created_at: Optional[AwareDatetime] = None
+    updated_at: Optional[AwareDatetime] = None
+
+
+class Config(BaseModel):
     class Config:
         # Заменяем стандартную работу с json на более быструю
         json_loads = orjson.loads
         json_dumps = orjson_dumps
+
+
+class Person(UuidMixin, DateMixin, Config):
+    full_name: str
+
+
+class Genre(UuidMixin, DateMixin, Config):
+    name: str
+    description: Optional[str] = None
+
+
+class Film(UuidMixin, DateMixin, Config):
+    title: str
+    description: Optional[str] = None
+    creation_date: Optional[date] = None
+    rating: Optional[float] = None
+    type: str
+    # связанные параметры
+    genre: Optional[list[str]] = None
+    director: Optional[str] = None
+    actors: Optional[list[Person]] = None
+    writers: Optional[list[Person]] = None
